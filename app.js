3874b7ee81dbe361f0c0fac172ddce8e34a115e4
PurpleAlma <64203600+PurpleAlma@users.noreply.github.com>
2020-09-03 21:45:45 +0300

    Update app.js
    fixed some listing problems(was adding infinite number of items to list)

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ fetch('https://swapi.dev/api/species')
         sullustan=(species.results[9])
 
         const keysHumans=Object.keys(human)
-
         const valuesHumans=Object.values(human)
     })
 
@@ -23,10 +22,25 @@ const human_attributes=document.getElementById('human-attributes')
 const droid_button=document.getElementById('droid-button')
 const droid_attributes=document.getElementById('droid-attributes')
 
-human_button.addEventListener('click',showHUmans)
-droid_button.addEventListener('click',showDroids)
+const showAllHumans=document.getElementById('show_all_humans_button')
 
-function showHUmans(){
+//BUTTON FOR SHOWING SOME HUMAN ATRIBUTES
+human_button.addEventListener('click',()=>{
+    let row=document.getElementById('human-row2')
+    row.innerHTML==''?showHumans():row.innerHTML=''
+})
+//BUTTON FOR SHOWING SOME DROID ATRIBUTES
+droid_button.addEventListener('click',()=>{
+    let row=document.getElementById('droid-row2')
+    row.innerHTML==''?showDroids():row.innerHTML=''
+})
+//BUTTON FOR SHOWING AAAALLLLLL HUMAN ATRIBUTES
+showAllHumans.addEventListener('click',()=>{
+    let mainlist=document.getElementById('show_all_humans_list')
+    mainlist.textContent==''?showAll():mainlist.textContent=''
+})
+//FUNCTION FOR SHOWING SOME HUMAN ATTRIBUTES
+function showHumans(){
     let humanval=Object.values(human)
     let myarray= [humanval[1],humanval[2],humanval[3],humanval[7]]
     let row2=document.getElementById('human-row2')    
@@ -34,9 +48,9 @@ function showHUmans(){
         let td=document.createElement('td')
         td.innerHTML=el
         row2.appendChild(td)
-       
     })
 }
+//FUNCTION FOR SHOWING SOME DROID ATTRIBUTES
 function showDroids(){
     let droidval=Object.values(droid)
     let myarray= [droidval[1],droidval[2],droidval[3],droidval[7]]
@@ -47,6 +61,7 @@ function showDroids(){
         row2.appendChild(td)
     })
 }
+//FUNCTION SHOWING ALL HUMANS
 function showAll(){
     let humanval=Object.values(human)
     let humankey=Object.keys(human)
@@ -67,9 +82,11 @@ function showAll(){
     */
    //this forEach goes over 2 arrays at the same time and puts them in a li
    humanval.forEach((hum,val)=>{
-   let mainlist=document.getElementById('all-human-values')
+   let mainlist=document.getElementById('show_all_humans_list')
    let newitem=document.createElement('li')
    newitem.innerHTML=humankey[val]+' : '+hum
    mainlist.appendChild(newitem)
 })
 }
+
+
